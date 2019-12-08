0ce77f22239da0ce5912c553e360211b2ee19f7f
Lucky Wirasakti <lucky.30@students.amikom.ac.id>
2019-12-08 21:00:02 +0700

    save

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -9,24 +9,10 @@ class HomeController extends Controller {
         $data['banner'] = $this->model('Home')->banner();
         $data['features'] = $this->model('Home')->feature();
         $data['brand'] = $this->model('Home')->brand();
+        $data['product'] = $this->model('Home')->product();
 
         $this->view('template/header', $data);
         $this->view('home', $data);
         $this->view('template/footer');
-    }
-
-    public function store()
-    {
-        
-    }
-
-    public function update()
-    {
-        
-    }
-
-    public function delete()
-    {
-        
     }
 }
